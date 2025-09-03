b7c33b75b6946ffc74af28b34885669049f4a975
Olivier <olivier.butler@gmail.com>
2025-09-02 19:35:20 -0700

    Bigger config

--- firmware/include/config_autogen.h
+++ firmware/include/config_autogen.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define SIDE_ID 0
-#define RUN_COUNT 1
-#define TOTAL_LED_COUNT 20
+#define RUN_COUNT 3
+#define TOTAL_LED_COUNT 1041
 #define PORT_BASE 49600
 #define STATUS_PORT 49700
 #define STATIC_IP_ADDR0 10
@@ -19,6 +19,8 @@
 #define STATIC_GW_ADDR3 1
 
 _Static_assert(RUN_COUNT <= 4, "RUN_COUNT exceeds 4");
-_Static_assert(20 <= 400, "LED_COUNT[0] exceeds 400");
+_Static_assert(362 <= 400, "LED_COUNT[0] exceeds 400");
+_Static_assert(300 <= 400, "LED_COUNT[1] exceeds 400");
+_Static_assert(379 <= 400, "LED_COUNT[2] exceeds 400");
 
-static const unsigned int LED_COUNT[RUN_COUNT] = {20};
+static const unsigned int LED_COUNT[RUN_COUNT] = {362, 300, 379};
